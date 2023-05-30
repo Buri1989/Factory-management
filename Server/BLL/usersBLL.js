18cfa00fa55bea2a27cd08734cafba2f31872777
Boris Sverdlov <Borkya@gmail.com>
2023-05-30 16:01:59 +0300

    Update usersBLL.js

--- Server/BLL/usersBLL.js
+++ Server/BLL/usersBLL.js
@@ -5,4 +5,34 @@ const getAllUsers = () => {
     return User.find({});
 };
 
-module.exports = { getAllUsers };
+/*Get user by id */
+const getUserById = async (id) => {
+    return await User.findById({ _id: id })
+}
+
+/*Create a new user  */
+const createNewUser = async (obj) => {
+    const user = User({
+        fullName: obj.fullName,
+        numOfActions: obj.createNewUser,
+    });
+    await user.save();
+    return 'Created!'
+};
+
+/*Update user */
+const updateUser = async (id, obj) => {
+    await User.findByIdAndUpdate(id, {
+        fullName: obj.fullName,
+        numOfActions: obj.createNewUser,
+    });
+    return 'Updated!'
+};
+
+/*Delete User */
+const deleteUser = async (id) => {
+    await User.findByIdAndDelete(id)
+    return 'Deleted'
+}
+
+module.exports = { getAllUsers, getUserById, createNewUser, updateUser, deleteUser };
