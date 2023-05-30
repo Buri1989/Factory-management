cc49326c79a1be1a57b4d6a7ccd33c7f3b5309f9
Boris Sverdlov <Borkya@gmail.com>
2023-05-30 17:03:46 +0300

    Update actionsFileBLL.js

--- Server/BLL/actionsFileBLL.js
+++ Server/BLL/actionsFileBLL.js
@@ -2,43 +2,18 @@ const actionsFile = require('../DAL/actionsFile');
 
 
 const getAllUsersDataFromFile = async () => {
-    const { actions } = await actionsFile.readFromFile();
+    const actions = await actionsFile.readFromFile();
     return actions;
 };
 
-/*Adding user data to file */
-const addUserDataToFile = async (obj) => {
-    const actions = await getAllUsersDataFromFile();
-    actions.push(obj);
-    const data = { actions };
-    const result = await actionsFile.writeToFile(data);
-    return result;
-};
+
 
 /*Update user data */
-const updateUserDataFromFile = async (id, obj) => {
-    const actions = await getAllUsersDataFromFile();
-    const index = actions.findIndex((action) => action.id === +id);
-    if (index !== -1) {
-        actions[index] = obj;
-        const data = { actions };
-        const result = await actionsFile.writeToFile(data);
-        return result;
-    }
-    return 'Wrong ID!';
+const updateUserDataFromFile = async (obj) => {
+    const actions = await actionsFile.writeToFile(obj);
+    return actions;
 };
 
-/*Delete user data */
-const deleteUserDataFromFile = async (id) => {
-    const actions = await getAllUsersDataFromFile();
-    const index = actions.findIndex((action) => action.id === +id);
-    if (index !== -1) {
-        actions.splice(index, 1);
-        const data = { actions };
-        const result = await actionsFile.writeToFile(data);
-        return result;
-    }
-    return 'Wrong ID!';
-};
 
-module.exports = { getAllUsersDataFromFile, addUserDataToFile, updateUserDataFromFile, deleteUserDataFromFile };
+
+module.exports = { getAllUsersDataFromFile, updateUserDataFromFile, };
