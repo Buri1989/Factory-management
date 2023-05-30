ffd3d9a4caa60d54dcdff72e7e5c89c9e405397d
Boris Sverdlov <Borkya@gmail.com>
2023-05-30 18:57:37 +0300

    update authRouter and delete actionsRouter

--- Server/routers/authRouter.js
+++ Server/routers/authRouter.js
@@ -1,40 +1,98 @@
 const express = require('express')
 const jwt = require('jsonwebtoken')
-
-const loginVerDAL = ('../DAL/loginVerifiedWS.js');
+const userLoginWS = require('../BLL/usersLoginWS');
+const authBLL = require('../BLL/authBLL');
 
 const router = express.Router();
 
-/*Entry Point 'http://localhost:8000/auth */
-
+/*Entry Point 'http://localhost:8000/auth/login */
 router.route('/login').post(async (req, res) => {
     const { username, email } = req.body;
-
-
-    /*if 'username' and 'email' are exist in https://jsonplaceholder.typicode.com/users*/
+    const ipAddress = req.socket.remoteAddress;
+    const RSA_PRIVATE_KEY = ipAddress;
 
     try {
-        const response = await loginVerDAL.getLoginData();
-        const ACCESS_SECRET_TOKEN = response.data.find(user => user.username === username && user.email === email);
+        const objResult = await userLoginWS.validateUserameAndEmail(username, email);
 
-        /*If a match is found, sign and return the JWT*/
-        if (ACCESS_SECRET_TOKEN) {
-            const accessToken = jwt.sign({
-                id: user.id,
-                username: user.username
-            },
-                secret, {
-                expiresIn: '1h'
-            },
-                ACCESS_SECRET_TOKEN
-            );
-            res.json(accessToken);
-        } else {
-            res.status(401).json({ error: 'Incorrect username or emil' });
+        /*Check if user exist */
+        if (Object.keys(objResult).length > 0) {
+            const userId = objResult[0].id;
+            const name = objResult[0].name;
+
+            const payload = {
+                userId,
+                name,
+                iat: Math.floor(Date.now() / 1000) - 30,
+            };
+
+            /*JWT Sigh - will use name and the id later when verify the token */
+            const tokenData = jwt.sign(payload, RSA_PRIVATE_KEY, { expiresIn: "24h" });
+
+            const isUserAllowed = await authBLL.loginAuthCheck(userId);
+            res.status(200).send({ token: tokenData, isUserAllowed });
         }
-    } catch (error) {
-        res.status(500).json({ error: 'Could not retrieve user data' });
+        else {
+            res.status(401).send('Invalid username or email');
+        }
     }
+    catch (err) {
+        consol.log(`The error is: ${err.errmsg}`)
+    }
+});
+
+/*Entry Point: 'http://localhost:8888/auth/userstatus */
+router.route('/userstatus').get(async (req, res) => {
+    const token = req.headers["x-access-token"];
+    if (!token) {
+        res.status(401).json("No Token Provided")
+    }
+
+    const RSA_PRIVATE_KEY = req.socket.remoteAddress;
+
+    jwt.verify(token, RSA_PRIVATE_KEY, async (err, data) => {
+        if (err) {
+            res.status(500).json("Failed to authenticate token");
+        }
+        try {
+            const userValidation = await authBLL.getUserCreditStatus(data.userId);
+
+            res.json({
+                currentUserFullName: data.name,
+                userId: data.userId,
+                creditNumber: userValidation.creditNum,
+            });
+        } catch (err) {
+            consol.log(`The error is: ${err.errmsg}`)
+        }
+    })
+});
+
+/*Entry Point: 'http://localhost:8888/auth/chargecredit' */
+router.route('/chargecredit').get(async (req, res) => {
+    const token = req.headers["x-access-token"];
+    if (!token) {
+        res.status(401).json("No Token Provided");
+    }
+
+    const RSA_PRIVATE_KEY = req.socket.remoteAddress;
+
+    jwt.verify(token, RSA_PRIVATE_KEY, async (err, data) => {
+        if (err) {
+            res.status(500).json("Failed to authenticate token");
+        }
+        try {
+            const userValidation = await authBLL.isUserHasCredit(data.userId);
+
+            res.json({
+                currentUserFullName: data.name,
+                userId: data.userId,
+                creditNumber: userValidation.creditNum,
+            });
+        } catch (err) {
+            consol.log(`The error is: ${err.errmsg}`)
+        }
+    })
+
 });
 
 module.exports = router
