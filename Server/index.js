77b5dcab27431a20fa60289a0e9f0c88f366918f
Boris Sverdlov <Borkya@gmail.com>
2023-06-03 11:41:01 +0300

    add CSS files

--- Server/index.js
+++ Server/index.js
@@ -1,15 +1,13 @@
 const express = require('express')
 const cors = require('cors')
+const path = require('path');
+
 const connectDB = require('./configs/factoryDB')
-
-
 const authRouter = require('./routers/authRouter');
-const welcomeRouter = require('./routers/welcomeRouter');
 const employeesRouter = require('./routers/employeesRouter');
 const departmentsRouter = require('./routers/departmentsRouter');
 const shiftsRouter = require('./routers/shiftsRouter');
 const usersRouter = require('./routers/usersRouter');
-const systemUserRouter = require('./routers/actionsRouter');
 
 const app = express();
 const port = 8000;
@@ -22,14 +20,13 @@ app.use(express.json());
 
 /*Routers */
 app.use('/auth', authRouter);
-app.use('/welcome', welcomeRouter);
 app.use('/employees', employeesRouter);
 app.use('/departments', departmentsRouter);
 app.use('/shifts', shiftsRouter);
 app.use('/users', usersRouter);
-app.use('/systemUser', systemUserRouter);
 
 
+app.use(express.static(path.join(__filename, 'public')));
 
 app.listen(port, () => {
     console.log(`app is listening at http://localhost:${port}`);
